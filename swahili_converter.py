944dedd808ff3014b424541b186e49fdeb932fff
Oyori Obegi <oyoriobegi18@gmail.com>
2023-06-29 23:39:48 +0300

    Update swahili_converter.py

--- swahili_converter.py
+++ swahili_converter.py
@@ -46,91 +46,75 @@ def number_to_swahili(n):
         else:
             return f'elfu {number_to_swahili(thousands)} {number_to_swahili(remainder)}'
 
-import re
 
-def swahili_to_number(s):
+def swahili_to_number(string):
     swahili_numbers = {
-        "moja": 1,
-        "mbili": 2,
-        "tatu": 3,
-        "nne": 4,
-        "tano": 5,
-        "sita": 6,
-        "saba": 7,
-        "nane": 8,
-        "tisa": 9,
-        "kumi": 10,
-        "ishirini": 20,
-        "thelathini": 30,
-        "arobaini": 40,
-        "hamsini": 50,
-        "sitini": 60,
-        "sabini": 70,
-        "themanini": 80,
-        "tisini": 90,
-        "mia": 100,
-        "elfu moja": 1000,
-        "elfu kumi": 10000
+    "moja": 1,
+    "mbili": 2,
+    "tatu": 3,
+    "nne": 4,
+    "tano": 5,
+    "sita": 6,
+    "saba": 7,
+    "nane": 8,
+    "tisa": 9,
+    "kumi": 10,
+    "ishirini": 20,
+    "thelathini": 30,
+    "arobaini": 40,
+    "hamsini": 50,
+    "sitini": 60,
+    "sabini": 70,
+    "themanini": 80,
+    "tisini": 90,
+    "mia moja": 100,
+    "elfu moja": 1000,
+    "elfu kumi": 10000
     }
+    words = string.split()
+    numerical_number = 0
 
-    words = s.split()
-    result = 0
-    temp_result = None
-    for i, word in enumerate(words):
-        if word in swahili_numbers:
-            number = swahili_numbers[word]
-            if temp_result is None:
-                temp_result = number
+    if string in swahili_numbers:
+        return swahili_numbers[string]
+
+    if words[0] in ["kumi", "ishirini", "thelathini", "arobaini", "hamsini", "sitini", "sabini", "themanini", "tisini"]:
+        if "na" in words:
+            index = words.index("na")
+            first_part = " ".join(words[:index])
+            second_part = words[index+1]
+            if first_part in swahili_numbers and second_part in swahili_numbers:
+                numerical_number += swahili_numbers[first_part] + swahili_numbers[second_part]
+                return numerical_number
+
+    if words[0] == "mia":
+        if words[1] != "moja":
+            second_word = words[1]
+            if "na" in words:
+                index = words.index("na")
+                third_word = words[index+1]
+                if second_word in swahili_numbers and third_word in swahili_numbers:
+                    numerical_number += swahili_numbers["mia moja"] * swahili_numbers[second_word] + swahili_numbers[third_word]
+                    return numerical_number
             else:
-                temp_result += number
-        elif word.startswith("kumi") and i < len(words) - 1 and words[i + 1] != "na":
-            tens_word = word.replace("kumi", "")
-            tens = swahili_numbers[tens_word]
-            temp_result = tens
-        elif word.startswith("mia") and i < len(words) - 1:
-            next_word = words[i + 1]
-            if next_word == "na":
-                temp_result = 0
+                if second_word in swahili_numbers:
+                    numerical_number += swahili_numbers["mia moja"] * swahili_numbers[second_word]
+                    return numerical_number
+
+    if words[0] == "elfu":
+        if words[1] != "moja":
+            second_word = words[1]
+            if "na" in words:
+                index = words.index("na")
+                third_word = words[index+1]
+                if second_word in swahili_numbers and third_word in swahili_numbers:
+                    numerical_number += swahili_numbers["elfu moja"] * swahili_numbers[second_word] + swahili_numbers[third_word]
+                    return numerical_number
             else:
-                hundreds = swahili_numbers[word]
-                if next_word in swahili_numbers:
-                    ones = swahili_numbers[next_word]
-                    temp_result = hundreds + ones
-                elif next_word.startswith("kumi") and i < len(words) - 2 and words[i + 2] != "na":
-                    tens_word = next_word.replace("kumi", "")
-                    tens = swahili_numbers[tens_word]
-                    temp_result = hundreds + tens
-        elif word.startswith("elfu") and i < len(words) - 1:
-            next_word = words[i + 1]
-            if next_word == "na":
-                temp_result = 0
-            else:
-                thousands = swahili_numbers[word]
-                if next_word in swahili_numbers:
-                    ones = swahili_numbers[next_word]
-                    temp_result = thousands + ones
-                elif next_word.startswith("kumi") and i < len(words) - 2 and words[i + 2] != "na":
-                    tens_word = next_word.replace("kumi", "")
-                    tens = swahili_numbers[tens_word]
-                    temp_result = thousands + tens
-                elif next_word == "elfu" and i < len(words) - 2 and words[i + 2] != "na":
-                    next_next_word = words[i + 2]
-                    if next_next_word in swahili_numbers:
-                        ones = swahili_numbers[next_next_word]
-                        temp_result = thousands * 1000 + ones
-                    elif next_next_word.startswith("kumi") and i < len(words) - 3 and words[i + 3] != "na":
-                        tens_word = next_next_word.replace("kumi", "")
-                        tens = swahili_numbers[tens_word]
-                        temp_result = thousands * 1000 + tens
+                if second_word in swahili_numbers:
+                    numerical_number += swahili_numbers["elfu moja"] * swahili_numbers[second_word]
+                    return numerical_number
 
-        if temp_result is not None and temp_result >= 1000:
-            result += temp_result
-            temp_result = None
-
-    if temp_result is not None:
-        result += temp_result
-
-    return result
+    return None
 
 
 
