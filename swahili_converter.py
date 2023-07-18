05ddf784f8c19adb2644096cef5313b875d23f0b
Oyori Obegi <oyoriobegi18@gmail.com>
2023-07-18 10:13:02 +0300

    Update swahili_converter.py
    Updated to use regex.

--- swahili_converter.py
+++ swahili_converter.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import re
 
 swahili_numbers = {
     0: 'sufuri',
@@ -20,26 +21,20 @@ swahili_numbers = {
     70: 'sabini',
     80: 'themanini',
     90: 'tisini',
+    100: 'mia moja'
 }
+
 def number_to_swahili(n):
     if n in swahili_numbers:
         return swahili_numbers[n]
-    
+
     if n < 100:
         tens, ones = divmod(n, 10)
         if ones == 0:
             return f'{swahili_numbers[tens * 10]}'
         else:
             return f'{swahili_numbers[tens * 10]} na {swahili_numbers[ones]}'
-    
-    if n < 1000:
-        hundreds, remainder = divmod(n, 100)
-        if remainder == 0:
-            return f'mia {number_to_swahili(hundreds)}'
-        else:
-            return f'mia {number_to_swahili(hundreds)} na {number_to_swahili(remainder)}'
-    
-    
+
 def swahili_to_number(string):
     number_mapping = {v: k for k, v in swahili_numbers.items()}
     words = string.lower().split()  # Convert the string to lowercase before splitting
@@ -48,17 +43,30 @@ def swahili_to_number(string):
     if string.lower() in number_mapping:  # Check the lowercase version of the string in the dictionary
         return number_mapping[string.lower()]
 
-    if words[0] in ["kumi", "ishirini", "thelathini", "arobaini", "hamsini", "sitini", "sabini", "themanini", "tisini"]:
-        if "na" in words:
-            index = words.index("na")
-            first_part = " ".join(words[:index])
-            second_part = words[index + 1]
-            if first_part in number_mapping and second_part in number_mapping:
-                numerical_number += number_mapping[first_part] + number_mapping[second_part]
-                return numerical_number
+    # Regular expression pattern to match numbers like 'kumi', 'ishirini', etc.
+    number_pattern = r'\b(?:kumi|ishirini|thelathini|arobaini|hamsini|sitini|sabini|themanini|tisini)\b'
+    # Regular expression pattern to match the separator 'na'
+    separator_pattern = r'\bna\b'
 
-   
-    return None
+    # Join the words together to form the input string
+    input_string = ' '.join(words)
+
+    # Use regular expressions to detect and extract the numerical values and separator
+    matches = re.findall(number_pattern, input_string)
+    separator_matches = re.findall(separator_pattern, input_string)
+
+    # Calculate the numerical number by summing up the extracted numerical values
+    numerical_number = sum(number_mapping[match] for match in matches)
+
+    # Check if 'na' is present, and adjust the numerical number accordingly
+    if len(separator_matches) == 1 and matches:
+        index = input_string.index('na')
+        prefix = input_string[:index].strip()
+        suffix = input_string[index + 2:].strip()
+        if prefix in number_mapping and suffix in number_mapping:
+            numerical_number += number_mapping[suffix]
+
+    return numerical_number
 
 def main():
     st.title("Swahili Number Converter")
@@ -81,9 +89,5 @@ def main():
     st.write("\n")  # Add vertical space
     st.write("Created by **Melinda Chebet and Oyori Obegi** for Theory of Computation CAT 2 2023")
 
-
 if __name__ == "__main__":
     main()
-
-
-
